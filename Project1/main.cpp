de4c180b8d247bb0762f03917ec9771c0e3902e9
epbugrov <bugrov102002@yandex.ru>
2025-02-19 21:30:22 +0300

    first commit

---
+++ Project1/main.cpp
@@ -0,0 +1,23 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+int main(int argc, char** argv)
+{
+	cv::namedWindow("Example3", cv::WINDOW_AUTOSIZE);
+
+	cv::VideoCapture cap;
+	cap.open("C:/Users/bugro/Videos/videoTest.mp4");
+
+	cv::Mat frame;
+	for (;;)
+	{
+		cap >> frame;
+		if (frame.empty())
+			break;
+
+		cv::imshow("Example3", frame);
+		if (cv::waitKey(33) >= 0)
+			break;
+	}
+	return 0;
+}
